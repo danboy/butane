673b878fe9031aa7ec3cffcee0891331a341fef5
Dan Nawara <dan@nawara.com>
2009-04-22 14:06:00 -0500

    hmm

--- butane.rb
+++ butane.rb
@@ -3,6 +3,10 @@ require 'rubygems'
 require 'tinder'
 require 'yaml'
 
+def notify(header,msg,options = {})
+  `notify-send #{options[:img]} #{options[:delay]} '#{header}' '#{msg}'`
+end
+
 def monitor_room(room, config = {})
   sticky = config[:sticky]
   room.listen do |m|
@@ -10,13 +14,13 @@ def monitor_room(room, config = {})
     # here
     next if m[:person].strip.empty?  # Ignore anything from a nil / empty person
 
-    delay = 5000 # in milliseconds (time to display the notification)
+    delay = "" # in milliseconds (time to display the notification)
 
-    # If we're to monitor something in particular in this room, set the 
+    # If we're to monitor something in particular in this room, set the
     # delay notification to zero which will leave the message up until
     # clicked away.
     if sticky
-      delay = 0 if m[:message] =~ /#{sticky}/i
+      delay = '-t 0' if m[:message] =~ /#{sticky}/i
     end
 
     img_opt = "-i #{config[:image]}" if config[:image]
@@ -31,8 +35,7 @@ def monitor_room(room, config = {})
     # And let's remove all but the href attribute in any anchors
     # in the msg.  Assumes that in href=stuff, stuff has no whitespace.
     msg.gsub! /<a[^>]+(href=[^\s]+)[^>]*>/, '<a \1>'
-
-    `notify-send -t #{delay} #{img_opt} \"#{m[:person]} in #{room.name}\" '#{msg}'`
+    notify(m[:person],msg,{:img => img_opt, :delay => delay})
   end
 end
 
@@ -44,7 +47,7 @@ account_names.each do |account_name|
   rooms = config[account_name][:rooms]
   account_img = config[account_name][:image]
   if rooms && rooms.size > 0
-    campfire = Tinder::Campfire.new account_name
+    campfire = Tinder::Campfire.new account_name, :ssl => config[account_name][:ssl]
     begin
       campfire.login config[account_name][:login], config[account_name][:password]
     rescue Tinder::Error => e
@@ -61,11 +64,14 @@ account_names.each do |account_name|
         room_cfg = rooms[room_name]
         room_cfg[:image] ||= account_img
         puts "  got it and monitoring."
+        img_opt = "-i #{config[account_name][:image]}" if config[account_name][:image]
+        notify(room_name,"is now being moitored",{:img => img_opt})
         threads << Thread.new(room, room_cfg) do |r, cfg|
           monitor_room(r, cfg)
         end
       else
         puts "  hmmm, didn't find it."
+        notify(room_name,"failed to connect",{:img => img_opt})
       end
     end
   end
